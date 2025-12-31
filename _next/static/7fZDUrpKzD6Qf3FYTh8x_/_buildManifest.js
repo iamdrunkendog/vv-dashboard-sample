9e95f9db749c3c98b5ba3c51629621f46a16fc82
김우람 <wramkim@gim-ulam-ui-MacBookAir.local>
2025-12-31 17:49:39 +0900

    Updates

--- _next/static/7fZDUrpKzD6Qf3FYTh8x_/_buildManifest.js
+++ _next/static/7fZDUrpKzD6Qf3FYTh8x_/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/vv-dashboard-sample//_next/:path+",
-        "destination": "/vv-dashboard-sample/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
